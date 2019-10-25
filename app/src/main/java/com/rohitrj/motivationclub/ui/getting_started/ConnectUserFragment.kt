3c37bcae02333b1c62755de7e766a69cdea5008b
Rohit Joshi <rohitj782@gmail.com>
2019-10-25 21:05:16 +0530

    implementing google sign in

--- app/src/main/java/com/rohitrj/motivationclub/ui/getting_started/ConnectUserFragment.kt
+++ app/src/main/java/com/rohitrj/motivationclub/ui/getting_started/ConnectUserFragment.kt
@@ -1,16 +1,31 @@
 package com.rohitrj.motivationclub.ui.getting_started
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.rohitrj.motivationclub.R
+import kotlinx.android.synthetic.main.fragment_connect_user.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
+private const val TAG = "ConnectUserFragment"
+private lateinit var auth: FirebaseAuth
+private const val RC_SIGN_IN = 101
+private lateinit var googleSignInClient: GoogleSignInClient
 class ConnectUserFragment : Fragment() {
 
     override fun onCreateView(
@@ -21,4 +36,64 @@ class ConnectUserFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_connect_user, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // Build a GoogleSignInClient with the options specified by gso.
+        googleSignInClient = GoogleSignIn.getClient(activity!!, gso);
+
+        buttonSignIn.setOnClickListener { signIn() }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.i(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.i(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+
+                // ...
+            }
+    }
+
 }
+
+
